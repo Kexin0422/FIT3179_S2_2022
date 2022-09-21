090964267503b41fd7f238d38ddc88808117f278
kexin22 <tankexin31@gmail.com>
2022-09-21 11:50:32 +0800

    added new file

---
+++ week7_tutorial_vegalite/Stacked bar chart (covid)/js/vega_lite_vis.js
@@ -0,0 +1,4 @@
+var spec3 = "js/stacked_bar_chart.vg.json";
+vegaEmbed('#stacked_bar_chart', spec3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
