2a08df6a1402a165e1c7c76116094afc132d9c77
kexin22 <tankexin31@gmail.com>
2022-09-21 17:52:14 +0800

    Updated files

---
+++ Project_2/js/vega_lite_vis.js
@@ -0,0 +1,4 @@
+var spec = "js/symbol_map.vg.json";
+vegaEmbed('#symbol_map', spec).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
