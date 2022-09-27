e5bbcdefe2e245a3d86b6926f4be1a21db1d5920
kexin22 <tankexin31@gmail.com>
2022-09-27 13:30:09 +0800

    Updated project 2

--- Project_2/js/vega_lite_vis.js
+++ Project_2/js/vega_lite_vis.js
@@ -1,4 +1,10 @@
 var spec = "js/symbol_map.vg.json";
+var spec2 = "js/stacked_bar_charts.vg.json";
+
 vegaEmbed('#symbol_map', spec).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed('#stacked_bar_chart', spec2).then(function(result) {
   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
