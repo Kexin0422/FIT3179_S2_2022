3f606e6710143df7e6cc3b7617e23646faa7c501
kexin22 <tankexin31@gmail.com>
2022-09-27 15:28:24 +0800

    Added code related to scatterplot rural area

--- Project_2/js/vega_lite_vis.js
+++ Project_2/js/vega_lite_vis.js
@@ -1,10 +1,15 @@
 var spec = "js/symbol_map.vg.json";
 var spec2 = "js/stacked_bar_charts.vg.json";
+var spec3 = "js/scatterplot_rural.vg.json";
 
 vegaEmbed('#symbol_map', spec).then(function(result) {
   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 vegaEmbed('#stacked_bar_chart', spec2).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed('#scatterplot_rural', spec3).then(function(result) {
   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
