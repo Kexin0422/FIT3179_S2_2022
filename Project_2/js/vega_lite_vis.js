8d9a13279f811d2f3adc513d1b4b980ee532b578
kexin22 <tankexin31@gmail.com>
2022-09-27 16:38:08 +0800

    Added code related to scatter plot urban

--- Project_2/js/vega_lite_vis.js
+++ Project_2/js/vega_lite_vis.js
@@ -1,6 +1,7 @@
 var spec = "js/symbol_map.vg.json";
 var spec2 = "js/stacked_bar_charts.vg.json";
 var spec3 = "js/scatterplot_rural.vg.json";
+var spec4 = "js/scatterplot_urban.vg.json";
 
 vegaEmbed('#symbol_map', spec).then(function(result) {
   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
@@ -11,5 +12,9 @@ vegaEmbed('#stacked_bar_chart', spec2).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed('#scatterplot_rural', spec3).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed('#scatterplot_urban', spec4).then(function(result) {
   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
