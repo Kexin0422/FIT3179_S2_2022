d80c427f486c08ba3a3bf40432d90dab1559ccd5
kexin22 <tankexin31@gmail.com>
2022-10-03 21:26:03 +0800

    Added week 10 hoemwork

---
+++ week10_homework/js/vega_lite_vis.js
@@ -0,0 +1,7 @@
+var spec = "js/symbol_map.vg.json";
+
+
+vegaEmbed('#symbol_map', spec).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
