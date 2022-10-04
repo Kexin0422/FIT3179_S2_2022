4e4bbb4b5d5f8f9cfdeee9a099ce4de277515239
kexin22 <tankexin31@gmail.com>
2022-10-04 17:27:04 +0800

    Added week 10 file

--- week10_homework/js/vega_lite_vis.js
+++ week10_homework/js/vega_lite_vis.js
@@ -1,7 +1,11 @@
 var spec = "js/symbol_map.vg.json";
+var spec2 = "js/stacked_bar_charts.vg.json";
 
 
 vegaEmbed('#symbol_map', spec).then(function(result) {
   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
+vegaEmbed('#stacked_bar_charts', spec2).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
