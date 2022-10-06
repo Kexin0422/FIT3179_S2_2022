b14c052d630db49cf0c35f6f62e8435cd63a5c95
kexin22 <tankexin31@gmail.com>
2022-10-06 21:59:38 +0800

    Updated project 2

--- Project_2/js/vega_lite_vis.js
+++ Project_2/js/vega_lite_vis.js
@@ -3,6 +3,7 @@ var spec2 = "js/stacked_bar_charts.vg.json";
 var spec3 = "js/dotplot_rural.vg.json";
 var spec4 = "js/dotplot_urban.vg.json";
 var spec5 = "js/line_chart.vg.json";
+var spec6 = "js/circle_packing.vg.json";
 
 vegaEmbed('#symbol_map', spec).then(function(result) {
   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
@@ -21,5 +22,9 @@ vegaEmbed('#dotplot_urban', spec4).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed('#line_chart', spec5).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed('#circle_packing', spec6).then(function(result) {
   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
